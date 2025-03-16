277b464c0703139744958f02a3d8bb23baa0a51a
Chris Legge <90413571+ChrisLegge@users.noreply.github.com>
2025-03-16 16:39:34 +0000

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ function showSidebar(){
   cont sidebar = document.querySelector('.sidebar')
   sidebar.style.display = 'flex'
 }
+function hideSidebar(){
+  cont sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
